2acf740e04f61bd064206fe0c9ec7ebb04519dfd
Marina Paley <marina_paley@mail.ru>
2021-10-27 14:54:38 +0300

    Начата работа над лр№2 #5

---
+++ Tasks/Task 2/01.sql
@@ -0,0 +1,12 @@
+--Найти максимальный размер из таблицы Production.Product
+SELECT MAX(Size) as SizeMax
+FROM Production.Product;
+
+SELECT MIN(Size) as SizeMax
+FROM Production.Product;
+
+--SELECT AVG(Size)
+--FROM Production.Product;
+
+SELECT COUNT(*) AS CountRows
+FROM Production.Product;
